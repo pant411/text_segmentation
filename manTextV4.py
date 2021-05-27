245280472905dbabdcac6c9702f50b3c1f84c924
pant411 <pantchanit2561@hotmail.com>
2021-05-27 14:02:53 +0700

    update manTextV4

--- manTextV4.py
+++ manTextV4.py
@@ -1,88 +1,81 @@
 #! -*- coding: UTF8 -*-
 from wordcut import Wordcut
 import pylcs
-import json
-
-#keyword = ['ส่วนราชการ','ที่','เรื่อง','วันที่','โทร.','ส่วนงาน','(นาย','(นาง','(นางสาว','(รศ. ดร.',
-# '(รองศาสตราจารย์ ดร.','(น.ส.','(รองศาสตราจารย์','(ผู้ช่วยศาสตราจารย์ ตร.','(ผศ. ดร.','(รศ.คร.','(ผศ.ดร.']
 
 def read_text():
     doc = input("file: ")
     data = open("docs/{}.txt".format(doc),"r")
     return data
 
-def main():
-    data = read_text()
-    org,tel,topic,toUser,byUser,date,no = [],[],[],[],[],[],[] #org=ส่วนงานหรือส่วนราชการ tel=เบอร์โทร topic=เรื่อง toUser=เรียน byUser=คนเซ็น date=วันที่ no=ที่ศธ
-    keyword = ['ส่วนราชการ','ส่วนงาน','เรื่อง','เรียน','วันที่','โทร.','ที่','นาย','นาง','นางสาว',
-               'น.ส.','รองศาสตราจารย์','ผู้ช่วยศาสตราจารย์','รศ.คร.','ผศ.ดร.','ดร.','ผศ.','รศ.']
-    line_no = 0
-    tag1,tag2,tag3 = [],[],[] #tag1=ภาค tag2=คณะ tag3=มหาวิทยาลัย
+def read_dict():
     with open('dict.txt', encoding="UTF-8") as dict_file:
         word_list = list(set([w.rstrip() for w in dict_file.readlines()]))
-        wordcut = Wordcut(word_list)
-        for line in data.readlines():
-            res = ''
-            inline = wordcut.tokenize(line)
-            inline = list(map(lambda s: s.strip(), inline))
-            inline.append('\n')
-            lock_keep = True #initial value
-            op = -1
-            for ele in inline:
-                candidate = pylcs.lcs_of_list(ele,keyword)
-                chosen = max(candidate)
-                indexOFchosen = candidate.index(chosen)
-                if 'ภาควิชา' in ele: tag1.append(ele)
-                if 'คณะ' in ele: tag2.append(ele)
-                if 'มหาวิทยาลัย' in ele: tag3.append(ele)
-                if (abs(len(keyword[indexOFchosen]) - chosen) <= 2) or '\n' in ele:
-                    #print(f'candidate is {candidate}, chosen is {chosen}, index is {indexOFchosen}, text is {keyword[indexOFchosen]}')
-                    #print(f'chosen: {inline}, {keyword[indexOFchosen]}')
-                    #print(f'ele: {ele}')
-                    if (lock_keep == False and line_no > 0) or '\n' in ele: 
-                        #print(f'op: {op}, res: {res}')        
-                        if op == 1: 
-                            org.append(res)
-                        elif op == 2: 
-                            topic.append(res)
-                        elif op == 3: 
-                            toUser.append(res)
-                        elif op == 4: 
-                            date.append(res)
-                        elif op == 5: 
-                            tel.append(res)
-                        elif op == 6: 
-                            no.append(res)
-                        elif op >= 7:
-                            byUser.append(res)
-                        res = ''
-                        lock_keep = True
-                    if lock_keep == True and '\n' not in ele:
-                        #print('gayray') 
-                        if indexOFchosen == 0 or indexOFchosen == 1: 
-                            op = 1 #collect in org
-                        elif indexOFchosen == 2: 
-                            op = 2 #keep in topic
-                        elif indexOFchosen == 3: 
-                            op = 3 #keep in toUser
-                        elif indexOFchosen == 4: 
-                            op = 4 #keep in date
-                        elif indexOFchosen == 5: 
-                            op = 5 #keep in tel
-                        elif indexOFchosen == 6: 
-                            op = 6 #keep in no
-                        elif indexOFchosen >= 7: 
-                            op = 7 #keep in byuser
-                        lock_keep = False
-                    line_no += 1
-                    continue             
-                if lock_keep == False and ele != ')':
-                    res = res + ele
-                    if (op == 7 or op == 4):
-                        res += ' '
-                #print(f'op: {op}, res: {res}')
-            line_no += 1  
-    #print(org,topic,toUser,byUser,tel,date,no)  
+        wordcut = Wordcut(word_list) 
+    return wordcut
+
+def select_tag(indexOFchosen):
+    if indexOFchosen <= 1: op = 1 #collect in org
+    elif indexOFchosen >= 7: op = 7 #keep in byuser   
+    else: op = indexOFchosen
+    return op 
+
+def store_tag(op,text,org,tel,topic,toUser,byUser,date,no):
+    if op == 1: org.append(text)
+    elif op == 2: topic.append(text)
+    elif op == 3: toUser.append(text)
+    elif op == 4: date.append(text)
+    elif op == 5: tel.append(text)
+    elif op == 6: no.append(text)
+    elif op >= 7: byUser.append(text)
+    return org,tel,topic,toUser,byUser,date,no
+
+def read_keyword():
+    my_file = open("keyword.txt", "r")
+    content = my_file.read()
+    content_list = content.split(",")
+    my_file.close()    
+    return content_list
+
+def test_tag(ele,tag1,tag2,tag3):
+    if 'ภาควิชา' in ele: tag1.append(ele)
+    if 'คณะ' in ele: tag2.append(ele)
+    if 'มหาวิทยาลัย' in ele: tag3.append(ele)    
+    return tag1,tag2,tag3
+
+def main():
+    data = read_text()
+    keyword = read_keyword()
+    line_no = 0
+    org,tel,topic,toUser,byUser,date,no = [],[],[],[],[],[],[] #org=ส่วนงานหรือส่วนราชการ tel=เบอร์โทร topic=เรื่อง toUser=เรียน byUser=คนเซ็น date=วันที่ no=ที่ศธ
+    tag1,tag2,tag3 = [],[],[] #tag1=ภาค tag2=คณะ tag3=มหาวิทยาลัย
+    wordcut = read_dict()
+    for line in data.readlines():
+        res = ''
+        inline = wordcut.tokenize(line)
+        inline = list(map(lambda s: s.strip(), inline))
+        inline.append('\n')
+        lock_store = True #initial value
+        op = -1
+        for ele in inline:
+            candidate = pylcs.lcs_of_list(ele,keyword)
+            chosen = max(candidate)
+            indexOFchosen = candidate.index(chosen)
+            tag1,tag2,tag3 = test_tag(ele,tag1,tag2,tag3)
+            if (abs(len(keyword[indexOFchosen]) - chosen) <= 2) or '\n' in ele:
+                if (lock_store == False and line_no > 0) or '\n' in ele:      
+                    org,tel,topic,toUser,byUser,date,no = store_tag(op,res,org,tel,topic,toUser,byUser,date,no)
+                    res = ''
+                    lock_store = True
+                if lock_store == True and '\n' not in ele:
+                    op = select_tag(indexOFchosen)
+                    lock_store = False
+                line_no += 1
+                continue             
+            if lock_store == False and ele != ')':
+                res = res + ele
+                if (op == 7 or op == 4):
+                    res += ' '
+        line_no += 1  
     print(f'org: {org}')
     print(f'topic: {topic}')
     print(f'toUser: {toUser}')

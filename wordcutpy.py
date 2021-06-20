22b865bdd202b28f6ddf3a7e11aa91c4b02bc4f3
pant411 <pantchanit2561@hotmail.com>
2021-06-20 13:19:55 +0700

    add dict_test

--- wordcutpy.py
+++ wordcutpy.py
@@ -1,8 +1,8 @@
 #! -*- coding: UTF8 -*-
 from wordcut import Wordcut
 if __name__ == '__main__':
-    with open('dict.txt', encoding="UTF-8") as dict_file:
+    with open('dict_test.txt', encoding="UTF-8") as dict_file:
         word_list = list(set([w.rstrip() for w in dict_file.readlines()]))
         wordcut = Wordcut(word_list)
-        print(wordcut.tokenize("ขอเรียนเชิญท่านผู้ทรงคุณวุฒิเข้าร่วมการวิพากษ์หลักสูตร"))
+        print(wordcut.tokenize("รักษาการแทนอธิการบดีมหาวิทยาลัยเกษตรศาสตร์ผ่านคณบดีคณะวิศวกรรมศาสตร์"))
         print(wordcut.tokenize("นเชิญท่านผู้ทรงคุณวุฒิเข้าร่วมการวิพากษ์หลักสูตร"))
